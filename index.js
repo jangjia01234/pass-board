1306a67034da345b11c6ebd97cc53755cfa65cda
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-09-18 01:06:08 +0900

    update css

---
+++ index.js
@@ -0,0 +1,9 @@
+// import "./style.css";
+
+// document.getElementById("app").innerHTML = `
+//   <h1>Hello Vanilla!</h1>
+// `;
+
+// document.getElementsByClassName("app").innerHTML = `
+//   <h1> test </h1>
+// `;
